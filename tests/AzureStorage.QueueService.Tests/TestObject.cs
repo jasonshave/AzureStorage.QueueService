54743c46d1863efae503884dc277c7d09c06f0a0
Jason Shave - MSFT <28457536+jasonshave@users.noreply.github.com>
2022-06-18 09:27:44 -0600

    added gh workflow (#3)
    * added gh workflow
    
    * fix warning
    
    * updated image file location
    
    * update test location
    
    * update tests location

--- tests/AzureStorage.QueueService.Tests/TestObject.cs
+++ tests/AzureStorage.QueueService.Tests/TestObject.cs
@@ -2,7 +2,7 @@
 
 public class TestObject
 {
-    public string Property1 { get; set; }
+    public string? Property1 { get; set; }
     public bool Property2 { get; set; }
     public int Property3 { get; set; }
 }
