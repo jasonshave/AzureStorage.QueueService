4128a6cfb7a3badd3ce312eb49c46d3b43cc99c4
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2023-05-17 07:26:19 -0600

    adding documentation and loggermessage

---
+++ src/AzureStorage.QueueService/Extensions/LoggerMessageExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace JasonShave.AzureStorage.QueueService.Extensions;
+
+public static partial class LoggerMessageExtensions
+{
+    [LoggerMessage(0, LogLevel.Information, "Processed message successfully, removing queue message id: {messageId}")]
+    public static partial void LogProcessedMessage(this ILogger logger, string messageId);
+
+    [LoggerMessage(1, LogLevel.Information, "Processing {messageCount} storage queue message(s).")]
+    public static partial void LogMessageCount(this ILogger logger, int messageCount);
+
+    [LoggerMessage(2, LogLevel.Error, "There was a problem sending the message. The error was: {errorMessage}")]
+    public static partial void LogSendError(this ILogger logger, string errorMessage);
+}
--- src/AzureStorage.QueueService/Services/AzureStorageQueueClient.cs
+++ src/AzureStorage.QueueService/Services/AzureStorageQueueClient.cs
@@ -32,6 +32,17 @@ public sealed class AzureStorageQueueClient
     public IEnumerable<TMessage> PeekMessages<TMessage>(int numMessages, CancellationToken cancellationToken = default) => 
         _queueClient.PeekMessages(numMessages, cancellationToken).Value.Convert<TMessage>(_messageConverter);
 
+    /// <summary>
+    /// Receives a message of the type specified and deserializes the input using a JSON message converter.
+    /// The message is processed using a function delegate and errors are processed using an exception handling delegate.
+    /// </summary>
+    /// <typeparam name="TMessage"></typeparam>
+    /// <param name="handleMessage"></param>
+    /// <param name="handleException"></param>
+    /// <param name="cancellationToken"></param>
+    /// <param name="numMessages"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
     public async ValueTask ReceiveMessagesAsync<TMessage>(Func<TMessage?, ValueTask> handleMessage, Func<Exception, ValueTask> handleException, CancellationToken cancellationToken = default, int numMessages = 1)
         where TMessage : class
     {
@@ -39,9 +50,12 @@ public sealed class AzureStorageQueueClient
 
         if (receivedMessages.Any())
         {
-            _logger.LogInformation("Processing {0} storage queue message(s).", receivedMessages.Length);
+            _logger.LogMessageCount(receivedMessages.Length);
 
-            List<QueueMessage> messages = receivedMessages.ToList();
+            foreach (var queueMessage in receivedMessages)
+            {
+                await ProcessMessage(queueMessage);
+            }
 
             async Task ProcessMessage(QueueMessage queueMessage)
             {
@@ -50,7 +64,7 @@ public sealed class AzureStorageQueueClient
                     var convertedMessage = _messageConverter.Convert<TMessage>(queueMessage.MessageText);
                     await handleMessage(convertedMessage);
 
-                    _logger.LogInformation("Processed message successfully, removing queue message id: {0}", queueMessage.MessageId);
+                    _logger.LogProcessedMessage(queueMessage.MessageId);
                     await _queueClient.DeleteMessageAsync(queueMessage.MessageId, queueMessage.PopReceipt, cancellationToken);
                 }
                 catch (Exception e)
@@ -58,19 +72,30 @@ public sealed class AzureStorageQueueClient
                     await handleException(e);
                 }
             }
-
-            foreach (var queueMessage in messages)
-            {
-                await ProcessMessage(queueMessage);
-            }
         }
     }
 
+    /// <summary>
+    /// Sends a message of a specified type and handles serialization of the message to the correct format.
+    /// </summary>
+    /// <typeparam name="TMessage"></typeparam>
+    /// <param name="message"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns><see cref="SendResponse"/></returns>
+    /// <exception cref="Exception"></exception>
     public async ValueTask<SendResponse> SendMessageAsync<TMessage>(TMessage message, CancellationToken cancellationToken = default)
     {
-        BinaryData binaryMessage = _messageConverter.Convert(message);
-        SendReceipt response = await _queueClient.SendMessageAsync(binaryMessage, null, null, cancellationToken);
+        try
+        {
+            BinaryData binaryMessage = _messageConverter.Convert(message);
+            SendReceipt response = await _queueClient.SendMessageAsync(binaryMessage, null, null, cancellationToken);
 
-        return new SendResponse(response.PopReceipt, response.MessageId);
+            return new SendResponse(response.PopReceipt, response.MessageId);
+        }
+        catch (Exception e)
+        {
+            _logger.LogSendError(e.Message);
+            throw;
+        }
     }
 }
